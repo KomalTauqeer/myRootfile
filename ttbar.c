a3349e969774aeaef89ee6facf59daf674455b3e
KomalTauqeer <komaltauqeer3@gmail.com>
2019-02-23 15:25:28 +0500

    updating files

--- ttbar.c
+++ ttbar.c
@@ -1,3 +1,4 @@
+#include <TMath.h>
 void ttbar()
 {
 	TFile *MyFile = new TFile("Electron_TTBar.root","READ");
@@ -5,13 +6,38 @@ void ttbar()
 	{
 			printf("File opened successfully\n");
 	}
-	MyFile -> ls();
-	//TFile MyFile("Electron_TTBar.root");
-	//if (MyFile.IsZombie()) {
-   //std::cout << "Error opening file" << std::endl;
-  // exit(-1);
-//}	
 
+	//TH1F *h = (TH1F*)gDirectory->Get("MCTightJetMul");
+	//h -> Draw();
+
+
+	TTree *t =(TTree*)MyFile->Get("SingleTop13TeVAnalyzer/MCTop");
+	t -> Draw("MCTopMass >> histogram");
+	TH1F *h =(TH1F*)gDirectory-> Get("histogram");
+
+	h->Draw("PE");
+	h->GetXaxis()->SetRangeUser(0,1000);
+
+	//TF1 *f= new TF1("f","[2]*(1/x)*(1/[1]*TMath::Sqrt(2*TMath::Pi()))*TMath::Exp(-1*(TMath::Power((TMath::Log(x)-[0]),2))/(2*TMath::Power([1],2)))",10,180);
+	//f->SetParameters(200,100,1000);
+	//h->Fit(f);
+
+	//TF1 *f= new TF1("f","([0]*TMath::Power(x,3))*(1/(TMath::Exp(([1]/[2])*x)-1))",10,180);
+	//f-> SetParameters(1500,500,900);
+	//h->Fit(f);
+	//gStyle -> SetOptFit(1111);
+
+	//TF1 *f= new TF1("f","((Exp(-1*[0])*(Power([0],x)))/(Factorial(x))",10,180);
+        //f-> SetParameters(400,500,900);
+        //h-> Fit(f);
+        //gStyle -> SetOptFit(1111);
+
+	TF1 *f = new TF1("f","landau",100,200);
+	TF1 *f1 = new TF1("f1","expo",150,800);
+        //f-> SetParameters(400,500,900);
+        h-> Fit(f,"R");
+	h-> Fit(f1,"R+");
+        gStyle -> SetOptFit(1111);
 
 
 }
